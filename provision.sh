93ef28030d33f947bf919a32bb88fc24fd44b2b7
tokko <andreg@kth.se>
2015-12-24 22:01:55 +0000

    updated provision.sh

--- provision.sh
+++ provision.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 sudo apt-get update
 sudo apt-get -y dist-upgrade
+sudo apt-get update
+sudo apt-get -y upgrade
 sudo apt-get install -y python transmission-daemon vim
 sudo easy_install pip
 sudo pip install flexget transmissionrpc
