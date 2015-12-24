6bd0b51fe70baaa7201935e4b74c6d481d30159c
tokko <andreg@kth.se>
2015-12-24 01:35:15 +0000

    panic fixes

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #/bin/bash
 git pull
-~/Flexget/provision.sh
-~/Flexget/make_folders.sh
+#/home/xbian/Flexget/provision.sh
+/home/xbian/Flexget/make_folders.sh
 flexget daemon stop
 sudo service transmission-daemon stop
 
-cp config.yml ~/.flexget
-perl -pi -e "s/TRAKT_USERNAME/$TRAKT_USERNAME/g" ~/.flexget/config.yml
-perl -pi -e "s/TRAKT_ACCOUNT/$TRAKT_ACCOUNT/g" ~/.flexget/config.yml
-perl -pi -e "s/TRANSMISSION_USERNAME/$TRANSMISSION_USERNAME/g" ~/.flexget/config.yml
-perl -pi -e "s/TRANSMISSION_PASSWORD/$TRANSMISSION_PASSWORD/g" ~/.flexget/config.yml
+cp /home/xbian/Flexget/config.yml /home/xbian/.flexget
+perl -pi -e "s/TRAKT_USERNAME/$TRAKT_USERNAME/g" /home/xbian/.flexget/config.yml
+perl -pi -e "s/TRAKT_ACCOUNT/$TRAKT_ACCOUNT/g" /home/xbian/.flexget/config.yml
+perl -pi -e "s/TRANSMISSION_USERNAME/$TRANSMISSION_USERNAME/g" /home/xbian/.flexget/config.yml
+perl -pi -e "s/TRANSMISSION_PASSWORD/$TRANSMISSION_PASSWORD/g" /home/xbian/.flexget/config.yml
 
-sudo cp settings.json /etc/transmission-daemon
+sudo cp /home/xbian/Flexget/settings.json /etc/transmission-daemon
 sudo perl -pi -e "s/TRANSMISSION_USERNAME/$TRANSMISSION_USERNAME/g" /etc/transmission-daemon/settings.json
 sudo perl -pi -e "s/TRANSMISSION_PASSWORD/$TRANSMISSION_PASSWORD/g" /etc/transmission-daemon/settings.json
 
