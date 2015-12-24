bc165add6db23b112787c9d8b997d00ea92b4125
tokko <andreg@kth.se>
2015-12-24 08:24:35 +0000

    fixed export script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #/bin/bash
-git pull
+git submodule update --recursive
 #/home/xbian/Flexget/provision.sh
+perl -pi -e "s/STORAGE/$DISK_NAME/g" /home/xbian/Flexget/make_folders.sh
 /home/xbian/Flexget/make_folders.sh
 flexget daemon stop
 sudo service transmission-daemon stop
@@ -10,7 +11,9 @@ perl -pi -e "s/TRAKT_USERNAME/$TRAKT_USERNAME/g" /home/xbian/.flexget/config.yml
 perl -pi -e "s/TRAKT_ACCOUNT/$TRAKT_ACCOUNT/g" /home/xbian/.flexget/config.yml
 perl -pi -e "s/TRANSMISSION_USERNAME/$TRANSMISSION_USERNAME/g" /home/xbian/.flexget/config.yml
 perl -pi -e "s/TRANSMISSION_PASSWORD/$TRANSMISSION_PASSWORD/g" /home/xbian/.flexget/config.yml
+perl -pi -e "s/STORAGE/$DISK_NAME/g" /home/xbian/.flexget/config.yml
 
+sudo cp /hom/xbian/Flexget/xbmc-upd.sh /etc/transmission-daemon
 sudo cp /home/xbian/Flexget/settings.json /etc/transmission-daemon
 sudo perl -pi -e "s/TRANSMISSION_USERNAME/$TRANSMISSION_USERNAME/g" /etc/transmission-daemon/settings.json
 sudo perl -pi -e "s/TRANSMISSION_PASSWORD/$TRANSMISSION_PASSWORD/g" /etc/transmission-daemon/settings.json
