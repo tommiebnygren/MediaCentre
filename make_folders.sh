ee4f7271af79c58525ba8ffaa9206b1841c6ba4e
tokko <andreg@kth.se>
2015-12-25 19:21:10 +0000

    support for dedicated subfolder on media deisk

--- make_folders.sh
+++ make_folders.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p ~/.flexget
-mkdir -p /media/$DISK_NAME/Downloads/incomplete
-mkdir -p /media/$DISK_NAME/TV\ Shows
-mkdir -p /media/$DISK_NAME/Movies
+mkdir -p /media/$DISK_NAME/$SUBFOLDER/Downloads/incomplete
+mkdir -p /media/$DISK_NAME/$SUBFOLDER/TV\ Shows
+mkdir -p /media/$DISK_NAME/$SUBFOLDER/Movies
