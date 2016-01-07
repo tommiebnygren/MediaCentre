471c4c1c812efb98ab165b861a516db6c810a7bc
tokko <andreg@kth.se>
2016-01-07 11:27:33 +0000

    correct ip for update server

--- export.sh
+++ export.sh
@@ -2,5 +2,5 @@
 export TRAKT_USERNAME=tokko
 export TRAKT_ACCOUNT=tokko
 export MEDIA_PATH=$HOME/Storage
-export UPDATE_IP=localhost #don't touch
+export UPDATE_IP=84.55.125.28 #don't touch
 export UPDATE_PORT=1337 #don't touch
