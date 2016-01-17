acb08f0b6e29d9384f26c243957c3d4ca8cecac1
gurrasson <andreas.gustafsson@beamonpeople.se>
2016-01-17 11:31:30 +0100

    update script

--- update.sh
+++ update.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 source ~/.flexget/export.sh
 
-echo "pulling transmission image"
 RES=sudo docker pull tokko/transmission:latest | grep "newer image"
-echo "pull complete"
 RES1=sudo docker ps --all | grep "transmission"
 if [ "$RES" != "" ] || [ "$RES1" == "" ] ; then
 	(sudo docker ps --all | grep "transmission") && sudo docker rm -f transmission 1>&2>/dev/null ;
-	echo "running transmission"
 	sudo docker run --restart=always -p 9091:9091 --name transmission -v $MEDIA_PATH:/media/Storage -v $HOME/.transmission:/var/lib/transmission-daemon tokko/transmission:latest &
 else
-	echo "just starting transmission"
 	sudo docker start transmission
 fi
 
-echo "pulling flexget"
 RES=sudo docker pull tokko/flexget:latest | grep "newer image" 
-echo "pull complete"
 RES1=sudo docker ps --all | grep "flexget"
 if [ "$RES" != "" ] || [ "$RES1" == "" ] ; then
-	echo "running flexget"
 	(sudo docker ps --all | grep "flexget") && sudo docker rm -f flexget;
-	echo "arg: $1"
 	sudo docker run --restart=always --name flexget --link transmission -e TRAKT_USERNAME=$TRAKT_USERNAME -e TRAKT_ACCOUNT=$TRAKT_ACCOUNT -v $HOME/.flexget:/root/.flexget -v $MEDIA_PATH:/root/Storage tokko/flexget:latest "/root/flexget.sh" "$1" &
 else
-	echo "just starting flexget"
 	sudo docker start flexget
 fi
+
+RES=sudo docker pull tokko/sonarr:latest | grep "newer image"
+RES1=sudo docker ps --all | grep "sonarr"
+if [ "$RES" != "" ] || [ "$RES1" == "" ] ; then
+	sudo docker run --restart=always -p 8989:8989 --name sonarr -v $MEDIA_PATH:/root/Storage -v $HOME/.sonarr:/root/.config/NzbDrone tokko/sonarr:latest &
+else
+	sudo docker start sonarr
+fi	
+
