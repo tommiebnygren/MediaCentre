a12a3e855ea9ad18ed8158b20c0e93c6492003d0
tokko <andreg@kth.se>
2016-01-24 20:49:26 +0000

    base image env removed

--- export.sh
+++ export.sh
@@ -4,5 +4,4 @@ echo "export TRAKT_ACCOUNT=tokko" >> ~/.bashrc
 echo "export MEDIA_PATH=/media/Elements" >> ~/.bashrc
 echo "export UPDATE_IP=84.55.125.28 #don't touch" >> ~/.bashrc
 echo "export UPDATE_PORT=1337 #don't touch" >> ~/.bashrc
-echo "export DOCKER_BASE_IMAGE=FROM resin/rpi-raspbian:jessie #don't touch" >> ~/.bashrc
 
