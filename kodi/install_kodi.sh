7e3808b10ffbd20439ca6d37b037695174d27367
tokko <andreg@kth.se>
2016-01-25 19:36:44 +0000

    fix of kodi

--- kodi/install_kodi.sh
+++ kodi/install_kodi.sh
@@ -20,9 +20,9 @@ sudo echo KERNEL==tty[0-9]*,            MODE=0666 > /etc/udev/rules.d/10-permiss
 sudo echo SUBSYSTEM==vchiq,  GROUP=video, MODE=066 > /etc/udev/rules.d/10-permissions.rules
 0
 
-sudo usermod -a -G audio pi
-sudo usermod -a -G video pi
-sudo usermod -a -G input pi
-sudo usermod -a -G dialout pi
-sudo usermod -a -G plugdev pi
-sudo usermod -a -G tty pi
+sudo usermod -a -G audio root
+sudo usermod -a -G video root
+sudo usermod -a -G input root
+sudo usermod -a -G dialout root
+sudo usermod -a -G plugdev root
+sudo usermod -a -G tty root
