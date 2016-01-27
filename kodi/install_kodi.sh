39f66cf33ae9c1b293fdc5d4410ddfacf36225d8
tokko <andreg@kth.se>
2016-01-27 23:33:59 +0000

    install_kodi

--- kodi/install_kodi.sh
+++ kodi/install_kodi.sh
@@ -5,24 +5,24 @@ tar -xzf kodi-15-jessie-rpb.tar.gz
 cd kodi-15-jessie
 ./install
 
-addgroup --system input
+sudo addgroup --system input
 
-echo "SUBSYSTEM==input, GROUP=input, MODE=0660" > /etc/udev/rules.d/99-input.rulessudo 
-echo "KERNEL==tty[0-9]*, GROUP=tty, MODE=0660" > /etc/udev/rules.d/99-input.rules
+sudo echo "SUBSYSTEM==input, GROUP=input, MODE=0660" > /etc/udev/rules.d/99-input.rulessudo 
+sudo echo "KERNEL==tty[0-9]*, GROUP=tty, MODE=0660" > /etc/udev/rules.d/99-input.rules
 
 # input
-echo KERNEL=="mouse*|mice|event*",   MODE="0660", GROUP="input" > /etc/udev/rules.d/10-permissions.rules
-echo KERNEL=="ts[0-9]*|uinput",     MODE="0660", GROUP="input" > /etc/udev/rules.d/10-permissions.rules
-echo KERNEL==js[0-9]*,             MODE=0660, GROUP=input > /etc/udev/rules.d/10-permissions.rules
+sudo echo KERNEL=="mouse*|mice|event*",   MODE="0660", GROUP="input" > /etc/udev/rules.d/10-permissions.rules
+sudo echo KERNEL=="ts[0-9]*|uinput",     MODE="0660", GROUP="input" > /etc/udev/rules.d/10-permissions.rules
+sudo echo KERNEL==js[0-9]*,             MODE=0660, GROUP=input > /etc/udev/rules.d/10-permissions.rules
 # tty
-echo KERNEL==tty[0-9]*,            MODE=0666 > /etc/udev/rules.d/10-permissions.rules
+sudo echo KERNEL==tty[0-9]*,            MODE=0666 > /etc/udev/rules.d/10-permissions.rules
 # vchiq
-echo SUBSYSTEM==vchiq,  GROUP=video, MODE=066 > /etc/udev/rules.d/10-permissions.rules
+sudo echo SUBSYSTEM==vchiq,  GROUP=video, MODE=066 > /etc/udev/rules.d/10-permissions.rules
 0
 
-sudo usermod -a -G audio root
-sudo usermod -a -G video root
-sudo usermod -a -G input root
-sudo usermod -a -G dialout root
-sudo usermod -a -G plugdev root
-sudo usermod -a -G tty root
+sudo usermod -a -G audio pi
+sudo usermod -a -G video pi
+sudo usermod -a -G input pi
+sudo usermod -a -G dialout pi
+sudo usermod -a -G plugdev pi
+sudo usermod -a -G tty pi
