74960add7af6a50e9436c98f8ecef6353f201a4a
tokko <andreg@kth.se>
2016-03-01 20:34:21 +0000

    tweaks

--- update.sh
+++ update.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-source ~/.bashrc
 
+echo "TRAKT_USER: $2"
+echo "MEDIA_PATH: $1"
 function data {
 	RES= sudo docker ps -a | grep Exit | grep $1_data
-	if [ "$RES" != ""] ; then
+	if [ "$RES" == "" ] ; then
 		sudo docker run --name $1_data tokko/$1:latest echo "$1 data container"
 	fi
 
@@ -14,7 +15,7 @@ RES=sudo docker pull tokko/transmission:latest | grep "newer image"
 RES1=sudo docker ps | grep "transmission"
 if [ "$RES" != "" ] || [ "$RES1" == "" ] ; then
 	(sudo docker ps | grep "transmission") && sudo docker rm -f transmission 1>&2>/dev/null ;
-	sudo docker run --restart=always -p 9091:9091 --privileged=true --volumes-from transmission_data --name transmission -v $MEDIA_PATH:/root/Storage tokko/transmission:latest &
+	sudo docker run --restart=always -p 9091:9091 --privileged=true --volumes-from transmission_data --name transmission -d -v $1:/root/Storage tokko/transmission:latest
 fi
 
 
@@ -24,7 +25,7 @@ RES1=sudo docker ps | grep "flexget"
 if [ "$RES" != "" ] || [ "$RES1" == "" ] ; then
 	echo "Starting flexget"
 	(sudo docker ps | grep "flexget") && sudo docker rm -f flexget;
-	sudo docker run --restart=always --privileged=true --name flexget --volumes-from flexget_data --link transmission -p 5050:5050 -e TRAKT_USERNAME=$TRAKT_USERNAME -v $MEDIA_PATH:/root/Storage tokko/flexget:latest "/root/flexget.sh" "$1" &
+	sudo docker run --restart=always --privileged=true --name flexget --volumes-from flexget_data --link transmission -p 5050:5050 -e TRAKT_USERNAME=$2 -v $1:/root/Storage -d tokko/flexget:latest "/root/flexget.sh" "$2" 
 fi
 
 data sickrage
@@ -33,6 +34,6 @@ RES1=sudo docker ps  | grep "sickrage"
 if [ "$RES" != "" ] || [ "$RES1" == "" ] ; then
 	echo "Starting sickrage"
 	(sudo docker ps | grep "sickrage") && sudo docker rm -f sickrage;
-	sudo docker run --privileged --link transmission -d --restart=always -p 8081:8081 --name sickrage --volumes-from sickrage_data -v $MEDIA_PATH:/root/Storage tokko/sickrage:latest
+	sudo docker run --privileged --link transmission -d --restart=always -p 8081:8081 --name sickrage --volumes-from sickrage_data -v $1:/root/Storage tokko/sickrage:latest
 fi	
 
