75025ca11bc340c2902a9080f25f7a54b8340c60
Tommie Nygren <tommie.nygren@gmail.com>
2016-04-22 15:41:08 +0200

    WHAT

--- install_host.sh
+++ install_host.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+#1
 sudo apt-get update
 sudo apt-get -y dist-upgrade
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y cron curl make kodi
+
+#2
 ./export.sh
+
+#3
 sudo perl -pi -e "s/ENABLED=0/ENABLED=1/g" /etc/default/kodi
 wget http://downloads.hypriot.com/docker-hypriot_1.9.1-1_armhf.deb
 sudo dpkg -i docker-hypriot_1.9.1-1_armhf.deb
@@ -12,13 +17,15 @@ sudo docker daemon 1>/dev/null &
 rm -f docker-hypriot_1.9.1-1_armhf.deb*
 sudo systemctl enable docker
 sudo gpasswd -a $USER docker
+
+#4
 sudo chown -R 777 $MEDIA_PATH
-sudo usermod -a -G audio kodi
-sudo usermod -a -G video kodi
-sudo usermod -a -G input kodi
-sudo usermod -a -G dialout kodi
-sudo usermod -a -G plugdev kodi
-sudo usermod -a -G tty kodi
+sudo usermod -a -G audio pi
+sudo usermod -a -G video pi
+sudo usermod -a -G input pi
+sudo usermod -a -G dialout pi
+sudo usermod -a -G plugdev pi
+sudo usermod -a -G tty pi
 #source ~/.bashrc
 
 #UPDATE_COMMAND="curl $UPDATE_IP:$UPDATE_PORT/update.sh | sh"
