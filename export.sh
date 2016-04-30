7ddee4c4df97fbe7ffa757da80045d4d2876f891
tommiebnygren <tommie.nygren@beamonpeople.se>
2016-04-30 08:40:45 +0200

    Uppdaterat mediapath

--- export.sh
+++ export.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo echo "export TRAKT_USERNAME=lavaeater" >> /etc/profile
 sudo echo "export TRAKT_ACCOUNT=lavaeater" >> /etc/profile
-sudo echo "export MEDIA_PATH=/media/pi/media" >> /etc/profile
-#sudo echo "export UPDATE_IP=84.55.125.28 #don't touch" >> /etc/profile
-#sudo echo "export UPDATE_PORT=1337 #don't touch" >> /etc/profile
+sudo echo "export MEDIA_PATH=/media/pp" >> /etc/profile
+sudo echo "export UPDATE_IP=84.55.125.28 #don't touch" >> /etc/profile
+sudo echo "export UPDATE_PORT=1337 #don't touch" >> /etc/profile
 #sudo echo "UUID=$1 /media/elements ntfs-3g auto,users,permissions 0 0" >> /etc/fstab
