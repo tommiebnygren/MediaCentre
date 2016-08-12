64b8ba14b59fa7b0d68a4d68d41e49b7b253ca24
tommiebnygren <tommie.nygren@beamonpeople.se>
2016-08-12 20:48:51 +0200

    Tog bort flexget

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "TRAKT_USER: $2"
+#echo "TRAKT_USER: $2"
 echo "MEDIA_PATH: $1"
 function data {
 	RES= sudo docker ps -a | grep Exit | grep $1_data
@@ -16,16 +16,6 @@ RES1=sudo docker ps | grep "transmission"
 if [ "$RES" != "" ] || [ "$RES1" == "" ] ; then
 	(sudo docker ps | grep "transmission") && sudo docker rm -f transmission 1>&2>/dev/null ;
 	sudo docker run --restart=always -p 9091:9091 --privileged=true --volumes-from transmission_data --name transmission -d -v $1:/root/Storage tokko/transmission:latest
-fi
-
-
-data flexget
-RES=sudo docker pull tokko/flexget:latest | grep "newer image" 
-RES1=sudo docker ps | grep "flexget"
-if [ "$RES" != "" ] || [ "$RES1" == "" ] ; then
-	echo "Starting flexget"
-	(sudo docker ps | grep "flexget") && sudo docker rm -f flexget;
-	sudo docker run --restart=always --privileged=true --name flexget --volumes-from flexget_data --link transmission -p 5050:5050 -e TRAKT_USERNAME=$2 -v $1:/root/Storage -d tokko/flexget:latest "/root/flexget.sh" "$2" 
 fi
 
 data sickrage
